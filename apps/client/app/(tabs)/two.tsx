007cbea9c98624033c8d29f1b143df443f3be6a4
Daniel Grychtoł <grychtol.daniel@gmail.com>
2023-12-02 16:50:00 +0100

    Cleanup

--- apps/client/app/(tabs)/two.tsx
+++ apps/client/app/(tabs)/two.tsx
@@ -1,6 +1,5 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
 export default function TabTwoScreen() {
@@ -8,7 +7,7 @@ export default function TabTwoScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <Text style={styles.title}>Client app</Text>
     </View>
   );
 }
--- apps/client/app/_layout.tsx
+++ apps/client/app/_layout.tsx
@@ -4,7 +4,6 @@ import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
-
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
