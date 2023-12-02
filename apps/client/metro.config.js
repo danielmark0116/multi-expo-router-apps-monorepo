9f2977640eada8e5ed0ceccf68f6dde871b658cd
Daniel Grychtoł <grychtol.daniel@gmail.com>
2023-12-02 13:35:21 +0100

    ⚙️  Add client app

---
+++ apps/client/metro.config.js
@@ -0,0 +1,33 @@
+// Learn more https://docs.expo.dev/guides/monorepos
+const { getDefaultConfig } = require('expo/metro-config');
+const { FileStore } = require('metro-cache');
+const path = require('path');
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
+
+const config = getDefaultConfig(projectRoot, {
+  isCSSEnabled: true,
+});
+
+// #1 - Watch all files in the monorepo
+config.watchFolders = [workspaceRoot];
+// #3 - Force resolving nested modules to the folders below
+// this is causing expo router failure
+// config.resolver.disableHierarchicalLookup = true;
+// #2 - Try resolving with project modules first, then workspace modules
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+
+// config.resolver.unstable_enablePackageExports = true;
+
+// config.resolver.sourceExts.unshift('mjs');
+
+// Use turborepo to restore the cache when possible
+config.cacheStores = [
+  new FileStore({ root: path.join(projectRoot, 'node_modules', '.cache', 'metro') }),
+];
+
+module.exports = config;
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Multi-expo-apps
+
+## Requirements:
+
+- [ ] Node 18.18.0
+- [ ] PNPM
+
+## Run and develop:
+
+1. Install packages with `pnpm install`
+2. Run given apps bundler with:
+
+```bash
+pnpm run dev:client
+// OR
+pnpm run dev:merchant
+```
+
+3. Code and preview with __Expo GO__ OR __dev client__
+4. In order to build dev client: `tbc`
+
+## Test suite:
+
+1. Run tests with `pnpm run test`
